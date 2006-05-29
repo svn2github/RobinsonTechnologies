e7f8b5508c74820aee418929af6679f0c8b5649d
mrfun <mrfun@353e56fe-9613-0410-8469-b96ad8e6f29c>
2006-05-29 14:08:52 +0000

    mac
    git-svn-id: svn://rtsoft.com/rtsvn@51 353e56fe-9613-0410-8469-b96ad8e6f29c

---
+++ clanlibstuff/novashell/scripts/mac_package_release.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+Pause()
+{
+    key=""
+    echo -n Hit any key to continue....
+    stty -icanon
+    key=`dd count=1 2>/dev/null`
+    stty icanon
+}
+
+echo Packing release
+cd ../mac
+mkdir dist
+
+#clean out the old one
+rm -R ./dist/novashell.app
+#copy in the new one
+echo Copying files to dist folder
+cp -R build/Default/novashell.app ./dist
+
+rm ./novashell.dmg
+#hdiutil create -volname "Novashell Disk Image" -srcfolder dist novashell.dmg
+#hdiutil internet-enable -yes novashell.dmg
+#return to the script directory
+
+echo Creating dmg
+make
+echo Done
+cd ../scripts
