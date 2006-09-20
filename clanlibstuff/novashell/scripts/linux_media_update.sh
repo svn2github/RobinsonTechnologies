ade50735058c679305b9dfb2cc68e19ba30cedf9
mrfun <mrfun@353e56fe-9613-0410-8469-b96ad8e6f29c>
2006-09-20 00:16:34 +0000

    linux
    git-svn-id: svn://rtsoft.com/rtsvn@148 353e56fe-9613-0410-8469-b96ad8e6f29c

--- clanlibstuff/novashell/scripts/linux_media_update.sh
+++ clanlibstuff/novashell/scripts/linux_media_update.sh
@@ -2,7 +2,9 @@
 
 #erase the old stuff
 echo Erasing local media directory
-rm -r ../bin/media
+rm -r ../bin/base
+rm -r ../bin/worlds
 
 echo Coping media tree from master server
-cp -r /media/projects/clanlibstuff/novashell/bin/media ../bin
+cp -r /media/projects/clanlibstuff/novashell/bin/base ../bin/base
+cp -r /media/projects/clanlibstuff/novashell/bin/worlds ../bin/worlds
