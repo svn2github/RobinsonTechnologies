19289b4751db685255e3239815c6ab4a1dcf954c
mrfun <mrfun@353e56fe-9613-0410-8469-b96ad8e6f29c>
2006-10-12 10:18:33 +0000

    git-svn-id: svn://rtsoft.com/rtsvn@163 353e56fe-9613-0410-8469-b96ad8e6f29c

--- clanlibstuff/novashell/scripts/mac_svn_update.sh
+++ clanlibstuff/novashell/scripts/mac_svn_update.sh
@@ -10,10 +10,29 @@ Pause()
 }
 
 echo Updating Clanlib from SVN
-cd ../../../../dev/ClanLib-0.8
-svn update
+
+#emergency copy system while clanlib.org is down
+
+#First check to see if our network drive is available
+
+if [ -d /Volumes/PROJECTS/clanlibstuff/novashell/bin/base ] 
+then
+
+ echo Located network drive.
+ else
+  echo -e "Network drive not available!!  Use Ctrl-K from finder to mount PROJECTS and CLANLIB\a"
+  Pause
+fi
+
+cd /Volumes/CLANLIB
+echo "Exporting CLANLIB..."
+svn export --force ./ ~/dev/Clanlib-0.8
+
+cd ~/dev/ClanLib-0.8
+#svn update
+
 echo Building clanlib...
-xcodebuild -configuration Deployment
+xcodebuild -project ClanLibUniversalBinaries.xcodeproj -configuration Deployment
 if [ $? -ne 0 ]
 then
 echo -e "Error building clanlib. \a"
