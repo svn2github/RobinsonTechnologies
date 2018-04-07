cb414fe9c3c9795d1169d2a03f0d4ba53b226b75
seth <seth@353e56fe-9613-0410-8469-b96ad8e6f29c>
2018-04-07 10:59:06 +0000

    Example of setting global wide vars, used in the latest html5 emscripten  builds
    git-svn-id: svn://rtsoft.com/rtsvn@1592 353e56fe-9613-0410-8469-b96ad8e6f29c

--- proton/base_setup.bat
+++ proton/base_setup.bat
@@ -1,1 +1,5 @@
+:This file is likely run by all project's app_info_setup.bat file, it allows you to set some global paths for all proton projects if needed.
+
 SET UNITY_EXE=C:\pro\unity5\Editor\Unity.exe
+SET PATH=C:\tools\Grsync\bin;%PATH%
+SET EMSCRIPTEN_ROOT=C:\pro\emscripten
